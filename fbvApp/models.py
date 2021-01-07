104ab9c9b4ac1d45aad3ae5b4a979a680c11076a
Roger Ccopa <rogerccopa@gmail.com>
2021-01-07 15:35:17 -0500

    Implemented Student Serializer

--- fbvApp/models.py
+++ fbvApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
+    score = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.id+self.name+self.score
--- fbvSerializers/settings.py
+++ fbvSerializers/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party
+    'rest_framework',
+    # custom apps
+    'fbvApp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,10 @@ WSGI_APPLICATION = 'fbvSerializers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'studentdb',
+        'USER': 'roger',
+        'PASSWORD': '2395d3161D!Ub'
     }
 }
 
